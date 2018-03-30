76b5aab19b6c9ca3330a539a6349ab6eccbb42a2
Avi Cavale <avi@shippable.com>
2018-03-30 06:44:20 -0700

    updating packer scripts

--- u16Image/basePack.sh
+++ u16Image/basePack.sh
@@ -43,7 +43,8 @@ build_ami() {
   echo "building AMI"
   echo "-----------------------------------"
 
-  packer build -machine-readable -var aws_access_key=$AWS_ACCESS_KEY_ID \
+  packer build -machine-readable \
+    -var aws_access_key=$AWS_ACCESS_KEY_ID \
     -var aws_secret_key=$AWS_SECRET_ACCESS_KEY \
     -var REGION=$REGION \
     -var VPC_ID=$VPC_ID \
